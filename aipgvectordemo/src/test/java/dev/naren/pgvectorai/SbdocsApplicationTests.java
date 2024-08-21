6b2e57fad0c627bd17ebef37e8a2f482bc2fef93
Narenakss <narendran.subburaj@gmail.com>
2024-08-20 23:50:39 -0400

    Add files via upload

---
+++ aipgvectordemo/src/test/java/dev/naren/pgvectorai/SbdocsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.naren.pgvectorai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbdocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
